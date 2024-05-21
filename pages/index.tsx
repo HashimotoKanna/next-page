5234eec7196f0d38a047e1eadaa1865b57b19375
HashimotoKanna <gmtn9200@gmail.com>
2024-05-22 02:12:36 +0900

    update: 成果物追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import ProgressBar from '../components/ProgressBar';
 import Button from '../components/Button'
 export default function Home() {
-  const [percentage, setPercentage] = useState(10); // 初期値を10%に設定
+  const [percentage, setPercentage] = useState(10); 
 
   const increaseProgress = () => {
     setPercentage((prev) => (prev < 100 ? prev + 10 : 100));
@@ -24,8 +24,29 @@ export default function Home() {
         <div className="content">
           <p>誕生日: 10月10日</p>
           <p>生い立ち: 石川県誕生 → 富山県引っ越し → 大阪府引っ越し → 富山引っ越し → 金沢工業大学入学</p>
-          <p>好きな食べ物:ジンジャーエール、ぶどう</p>
+          <p>好きな食べ物:ぶどう</p>
           <p>趣味: 音楽鑑賞、旅行、ゲーム</p>
+        </div>
+        <div className="content">
+          <h1>主な制作物</h1>
+          <p>すれ違い合戦: Dis gassen</p>
+          <div className='image-container'>
+            <img src= {"/static/screenshots/disgassen.png"} style={{ width: 500, height: 500 }} />
+          </div>
+          <p>採掘ゲーム: Ohana</p>
+          <div className='image-container'>
+            <img src= {"/static/screenshots/ohana.png"} style={{ width: 500, height: 500 }} />
+          </div>
+          <p>ゲームアシスタントBot: Taoる</p>
+          <div className='image-container'>
+            <img src= {"/static/screenshots/taoru.png"} style={{ width: 500, height: 500 }} />
+          </div>
+        </div>
+        <div className="content">
+          <img src= {"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
+          <p>Python</p>
+          <ProgressBar percentage={percentage} />
+          <Button text="click me" onClick={increaseProgress}/>
         </div>
         <div className="content">
           <img src= {"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
