dd1b9a4f3344e94585b1da9769ff4c679e29a8d6
HashimotoKanna <63573845+HashimotoKanna@users.noreply.github.com>
2024-05-22 02:25:57 +0900

    update: index.js
    バーの追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,28 @@ import Head from 'next/head';
 import { useState } from 'react';
 import ProgressBar from '../components/ProgressBar';
 import Button from '../components/Button'
-export default function Home() {
-  const [percentage, setPercentage] = useState(10); 
 
-  const increaseProgress = () => {
-    setPercentage((prev) => (prev < 100 ? prev + 10 : 100));
+export default function Home() {
+  const [pythonPercentage, setPythonPercentage] = useState(10);
+  const [javascriptPercentage, setJavascriptPercentage] = useState(10);
+  const [javaPercentage, setJavaPercentage] = useState(10);
+  const [cPercentage, setCPercentage] = useState(10);
+  const [mysqlPercentage, setMysqlPercentage] = useState(10);
+
+  const pythonProgress = () => {
+    setPythonPercentage((prev) => (prev < 100 ? prev + 10 : 100));
+  };
+  const javascriptProgress = () => {
+    setJavascriptPercentage((prev) => (prev < 100 ? prev + 10 : 100));
+  };
+  const javaProgress = () => {
+    setJavaPercentage((prev) => (prev < 100 ? prev + 10 : 100));
+  };
+  const cProgress = () => {
+    setCPercentage((prev) => (prev < 100 ? prev + 10 : 100));
+  };
+  const mysqlProgress = () => {
+    setMysqlPercentage((prev) => (prev < 100 ? prev + 10 : 100));
   };
 
   return (
@@ -31,52 +48,46 @@ export default function Home() {
           <h1>主な制作物</h1>
           <p>すれ違い合戦: Dis gassen</p>
           <div className='image-container'>
-            <img src= {"/static/screenshots/disgassen.png"} style={{ width: 500, height: 500 }} />
+            <img src={"/static/screenshots/disgassen.png"} style={{ width: 500, height: 500 }} />
           </div>
           <p>採掘ゲーム: Ohana</p>
           <div className='image-container'>
-            <img src= {"/static/screenshots/ohana.png"} style={{ width: 500, height: 500 }} />
+            <img src={"/static/screenshots/ohana.png"} style={{ width: 500, height: 500 }} />
           </div>
           <p>ゲームアシスタントBot: Taoる</p>
           <div className='image-container'>
-            <img src= {"/static/screenshots/taoru.png"} style={{ width: 500, height: 500 }} />
+            <img src={"/static/screenshots/taoru.png"} style={{ width: 500, height: 500 }} />
           </div>
         </div>
         <div className="content">
-          <img src= {"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
+          <img src={"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
           <p>Python</p>
-          <ProgressBar percentage={percentage} />
-          <Button text="click me" onClick={increaseProgress}/>
+          <ProgressBar percentage={pythonPercentage} />
+          <Button text="click me" onClick={pythonProgress} />
         </div>
         <div className="content">
-          <img src= {"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
-          <p>Python</p>
-          <ProgressBar percentage={percentage} />
-          <Button text="click me" onClick={increaseProgress}/>
-        </div>
-        <div className="content">
-          <img src= {"/static/techs/javascript_icon.png"} style={{ width: 50, height: 50 }} />
+          <img src={"/static/techs/javascript_icon.png"} style={{ width: 50, height: 50 }} />
           <p>JavaScript</p>
-          <ProgressBar percentage={percentage} />
-          <Button text="click me" onClick={increaseProgress}/>
+          <ProgressBar percentage={javascriptPercentage} />
+          <Button text="click me" onClick={javascriptProgress} />
         </div>
         <div className="content">
-          <img src= {"/static/techs/java_icon.png"} style={{ width: 50, height: 50 }} />
+          <img src={"/static/techs/java_icon.png"} style={{ width: 50, height: 50 }} />
           <p>Java</p>
-          <ProgressBar percentage={percentage} />
-          <Button text="click me" onClick={increaseProgress}/>
+          <ProgressBar percentage={javaPercentage} />
+          <Button text="click me" onClick={javaProgress} />
         </div>
         <div className="content">
-          <img src= {"/static/techs/c_icon.png"} style={{ width: 50, height: 50 }} />
+          <img src={"/static/techs/c_icon.png"} style={{ width: 50, height: 50 }} />
           <p>C</p>
-          <ProgressBar percentage={percentage} />
-          <Button text="click me" onClick={increaseProgress}/>
+          <ProgressBar percentage={cPercentage} />
+          <Button text="click me" onClick={cProgress} />
         </div>
         <div className="content">
-          <img src= {"/static/techs/mysql_icon.png"} style={{ width: 50, height: 50 }} />
+          <img src={"/static/techs/mysql_icon.png"} style={{ width: 50, height: 50 }} />
           <p>MySQL</p>
-          <ProgressBar percentage={percentage} />
-          <Button text="click me" onClick={increaseProgress}/>
+          <ProgressBar percentage={mysqlPercentage} />
+          <Button text="click me" onClick={mysqlProgress} />
         </div>
       </div>
     </div>
