e6b07f1a66aa0244fad4a10cf25d7b7d67251358
HashimotoKanna <63573845+HashimotoKanna@users.noreply.github.com>
2024-05-25 01:32:36 +0900

    update: index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import Head from 'next/head';
 import { useState } from 'react';
 import ProgressBar from '../components/ProgressBar';
 import Button from '../components/Button';
-import styles from '../styles/Home.module.css'; // 新しいCSSファイルをインポート
+import styles from '../styles/Home.module.css'; // CSSモジュールをインポート
 
 export default function Home() {
-  const [pythonPercentage, setPythonPercentage] = useState(10);
-  const [javascriptPercentage, setJavascriptPercentage] = useState(10);
-  const [javaPercentage, setJavaPercentage] = useState(10);
+  const [pythonPercentage, setPythonPercentage] = useState(50);
+  const [javascriptPercentage, setJavascriptPercentage] = useState(60);
+  const [javaPercentage, setJavaPercentage] = useState(20);
   const [cPercentage, setCPercentage] = useState(10);
-  const [mysqlPercentage, setMysqlPercentage] = useState(10);
+  const [mysqlPercentage, setMysqlPercentage] = useState(40);
+
 
   const pythonProgress = () => {
     setPythonPercentage((prev) => (prev < 100 ? prev + 10 : 100));
@@ -18,15 +19,7 @@ export default function Home() {
   const javascriptProgress = () => {
     setJavascriptPercentage((prev) => (prev < 100 ? prev + 10 : 100));
   };
-  const javaProgress = () => {
-    setJavaPercentage((prev) => (prev < 100 ? prev + 10 : 100));
-  };
-  const cProgress = () => {
-    setCPercentage((prev) => (prev < 100 ? prev + 10 : 100));
-  };
-  const mysqlProgress = () => {
-    setMysqlPercentage((prev) => (prev < 100 ? prev + 10 : 100));
-  };
+
 
   return (
     <div>
@@ -46,23 +39,23 @@ export default function Home() {
           <table className={styles.table}>
             <tbody>
               <tr>
-                <td>2003年10月</td>
+                <td>20xx年yy月</td>
                 <td>石川県誕生</td>
               </tr>
               <tr>
-                <td>2004年8月</td>
+                <td>20xx年yy月</td>
                 <td>富山県引っ越し</td>
               </tr>
               <tr>
-                <td>2013年8月</td>
+                <td>20xx年yy月</td>
                 <td>大阪府引っ越し</td>
               </tr>
               <tr>
-                <td>2016年5月</td>
+                <td>20xx年yy月</td>
                 <td>富山県引っ越し</td>
               </tr>
               <tr>
-                <td>2022年4月</td>
+                <td>20xx年yy月</td>
                 <td>金沢工業大学入学</td>
               </tr>
             </tbody>
@@ -90,35 +83,32 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="content">
-          <img src={"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
-          <p>Python</p>
-          <ProgressBar percentage={pythonPercentage} />
-          <Button text="click me" onClick={pythonProgress} />
-        </div>
-        <div className="content">
-          <img src={"/static/techs/javascript_icon.png"} style={{ width: 50, height: 50 }} />
-          <p>JavaScript</p>
-          <ProgressBar percentage={javascriptPercentage} />
-          <Button text="click me" onClick={javascriptProgress} />
-        </div>
-        <div className="content">
-          <img src={"/static/techs/java_icon.png"} style={{ width: 50, height: 50 }} />
-          <p>Java</p>
-          <ProgressBar percentage={javaPercentage} />
-          <Button text="click me" onClick={javaProgress} />
-        </div>
-        <div className="content">
-          <img src={"/static/techs/c_icon.png"} style={{ width: 50, height: 50 }} />
-          <p>C</p>
-          <ProgressBar percentage={cPercentage} />
-          <Button text="click me" onClick={cProgress} />
-        </div>
-        <div className="content">
-          <img src={"/static/techs/mysql_icon.png"} style={{ width: 50, height: 50 }} />
-          <p>MySQL</p>
-          <ProgressBar percentage={mysqlPercentage} />
-          <Button text="click me" onClick={mysqlProgress} />
+        <div className={styles.grid}>
+          <div className="content">
+            <img src={"/static/techs/python_icon.png"} style={{ width: 50, height: 50 }} />
+            <p>Python</p>
+            <ProgressBar percentage={pythonPercentage} />
+          </div>
+          <div className="content">
+            <img src={"/static/techs/javascript_icon.png"} style={{ width: 50, height: 50 }} />
+            <p>JavaScript</p>
+            <ProgressBar percentage={javascriptPercentage} />
+          </div>
+          <div className="content">
+            <img src={"/static/techs/java_icon.png"} style={{ width: 50, height: 50 }} />
+            <p>Java</p>
+            <ProgressBar percentage={javaPercentage} />
+          </div>
+          <div className="content">
+            <img src={"/static/techs/c_icon.png"} style={{ width: 50, height: 50 }} />
+            <p>C</p>
+            <ProgressBar percentage={cPercentage} />
+          </div>
+          <div className="content">
+            <img src={"/static/techs/mysql_icon.png"} style={{ width: 50, height: 50 }} />
+            <p>MySQL</p>
+            <ProgressBar percentage={mysqlPercentage} />
+          </div>
         </div>
       </div>
     </div >
